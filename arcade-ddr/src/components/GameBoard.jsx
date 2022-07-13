95e7287ab27142f0cf4d87de2e875bb458d90776
Iffat Hossain <iffathossain77@gmail.com>
2022-07-13 17:23:20 -0400

    adds component changes needs to be revised

--- arcade-ddr/src/components/GameBoard.jsx
+++ arcade-ddr/src/components/GameBoard.jsx
@@ -1,14 +1,18 @@
-import React from 'react'
+import React from 'react';
+import Cell from './Cell';
+import './gameboard.css';
 
 export default function GameBoard() {
 
 
-
   return (
     <div>
+      <div className='board-title'> TTR </div>
       <div className='game-board-container'>
-        <div className='board-title'> TTR </div>
+        <div className='the-board'>
 
+
+        </div>
 
     </div>
     </div>
